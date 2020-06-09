86484b73edfd6228e9572b0ad4a51a522e8bf624
RedSeock86 <redseock@redseok.com>
2020-06-09 20:18:23 +0900

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,7 @@ class Mypage extends StatelessWidget {
         title: Text('Appbar icon menu'),
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button is cliked');
-          },
-        ),
+
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -46,6 +41,65 @@ class Mypage extends StatelessWidget {
           ),
         ],
       ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('assets/icon_one.png'),
+                ),
+                otherAccountsPictures: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                  ),
+                ],
+                accountName: Text('BBANTO'),
+                accountEmail: Text('jovajjs28'),
+                onDetailsPressed:(){//버튼을 클릭하면 밑으로 화면을 보여준다
+                  print('arrow is cliked');
+                } ,
+                decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home,
+                  color: Colors.grey[850],
+                ),
+                title: Text('home'),
+                onTap: (){
+                  print('home is clieked');
+                },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings,
+                  color: Colors.grey[850],
+                ),
+                title: Text('settings'),
+                onTap: (){
+                  print('home is settings');
+                },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(Icons.question_answer,
+                  color: Colors.grey[850],
+                ),
+                title: Text('Q&A'),
+                onTap: (){
+                  print('home is question_answer');
+                },
+                trailing: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
     );
   }
 }
