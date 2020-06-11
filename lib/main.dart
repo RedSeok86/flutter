59765fb54b31ccc0c010c6613808055cf3981a4f
RedSeock86 <redseock@redseok.com>
2020-06-11 19:09:21 +0900

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -8,98 +8,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'AppBar',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
-      home: Mypage(),
+     home: MyPage(),
     );
+    
   }
 }
 
-
-class Mypage extends StatelessWidget {
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appbar icon menu'),
+        title: Text('Snack Bar'),
         centerTitle: true,
-        elevation: 0.0,
-
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('menu button is shopping_cart');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('menu button is search');
-            },
-          ),
-        ],
       ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage('assets/icon_one.png'),
-                ),
-                otherAccountsPictures: <Widget>[
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                  ),
-                ],
-                accountName: Text('BBANTO'),
-                accountEmail: Text('jovajjs28'),
-                onDetailsPressed:(){//버튼을 클릭하면 밑으로 화면을 보여준다
-                  print('arrow is cliked');
-                } ,
-                decoration: BoxDecoration(
-                  color: Colors.red[200],
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40.0),
-                    bottomRight: Radius.circular(40.0),
-                  ),
-                ),
+      body: Builder(builder: (BuildContext ctx) {
+        return Center(
+          child: FlatButton( // 화초면에 그려진다
+            child: Text('show me',
+              style: TextStyle(
+                  color: Colors.white
               ),
-              ListTile(
-                leading: Icon(Icons.home,
-                  color: Colors.grey[850],
-                ),
-                title: Text('home'),
-                onTap: (){
-                  print('home is clieked');
-                },
-                trailing: Icon(Icons.add),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings,
-                  color: Colors.grey[850],
-                ),
-                title: Text('settings'),
-                onTap: (){
-                  print('home is settings');
-                },
-                trailing: Icon(Icons.add),
-              ),
-              ListTile(
-                leading: Icon(Icons.question_answer,
-                  color: Colors.grey[850],
-                ),
-                title: Text('Q&A'),
-                onTap: (){
-                  print('home is question_answer');
-                },
-                trailing: Icon(Icons.add),
-              ),
-            ],
+            ),
+            color: Colors.red,
+            onPressed: (){
+              Scaffold.of(ctx).showSnackBar(SnackBar(content: Text('hellow')
+              ));
+            },
           ),
-        ),
+        );
+      },)
     );
   }
 }
